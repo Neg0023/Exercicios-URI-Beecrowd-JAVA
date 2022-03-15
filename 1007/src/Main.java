a3a3e481856bf3ba60c07bd2fae800c1e5c76817
Uemerson Santos <uemerson161@hotmail.com>
2022-03-15 12:04:53 -0300

    Exercicios do 1005 ao 1009

---
+++ 1007/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public  static void main(String[] args){
+        //Leia quatro valores inteiros A, B, C e D.
+        // A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D
+        // segundo a fórmula: DIFERENCA = (A * B - C * D).
+        //
+        //Entrada
+        //O arquivo de entrada contém 4 valores inteiros.
+        //
+        //Saída
+        //Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo,
+        // com um espaço em branco antes e depois da igualdade.
+
+        Scanner sc = new Scanner(System.in);
+        int a, b, c, d, diferenca;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+
+        diferenca = ((a*b) - (c*d));
+
+        System.out.printf("DIFERENCA = %d%n", diferenca);
+
+        sc.close();
+    }
+}
---
+++ 1009/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas
+        // por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
+        // informar o total a receber no final do mês, com duas casas decimais.
+        //
+        //Entrada
+        //O arquivo de entrada contém um texto (primeiro nome do vendedor)
+        // e 2 valores de dupla precisão (double) com duas casas decimais,
+        // representando o salário fixo do vendedor
+        // e montante total das vendas efetuadas por este vendedor, respectivamente.
+        //
+        //Saída
+        //Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        double  salario, vendas, total;
+
+        nome = sc.nextLine();
+        salario = sc.nextDouble();
+        vendas = sc.nextDouble();
+
+        total = salario + vendas*0.15;
+
+        System.out.printf("TOTAL = R$ %.2f%n", total);
+
+        sc.close();
+    }
+}
