e234bc349110d6406f02528dda54feaaec1dc7f2
Uemerson Santos <uemerson161@hotmail.com>
2022-03-28 23:07:24 -0300

    Exercicio 1021 corrigido

--- 1021/src/Main.java
+++ 1021/src/Main.java
@@ -18,31 +18,62 @@ public class Main {
         //Obs: Utilize ponto (.) para separar a parte decimal.
         Scanner sc = new Scanner(System.in);
         double entrada = sc.nextDouble();
-        double nota100, nota50, nota20, nota10, nota5, nota2, nota1, resto;
+        double resto;
 
-        nota100 = entrada / 100;
-        resto = entrada % 100;
-        nota50 = resto / 50;
-        resto = resto % 50;
-        nota20 = resto / 20;
-        resto = resto % 20;
-        nota10 = resto / 10;
-        resto = resto % 10;
-        nota5 = resto / 5;
-        resto = resto % 5;
-        nota2 = resto / 2;
-        resto = resto % 2;
-        nota1 = resto;
-        nota100 = (int) nota100;
+        System.out.println("NOTAS:");
+        int entradaInteiro = 0;
+        entradaInteiro = (int) entrada / 100;
+        System.out.printf("%d nota(s) de R$ 100.00%n", entradaInteiro);
+        entrada = entrada % 100;
 
-        System.out.println(entrada);
-        System.out.printf("%d nota(s) de R$ 100,00%n", nota100);
-        System.out.printf("%d nota(s) de R$ 50,00%n", nota50);
-        System.out.printf("%d nota(s) de R$ 20,00%n", nota20);
-        System.out.printf("%d nota(s) de R$ 10,00%n", nota10);
-        System.out.printf("%d nota(s) de R$ 5,00%n", nota5);
-        System.out.printf("%d nota(s) de R$ 2,00%n", nota2);
-        System.out.printf("%d nota(s) de R$ 1,00%n", nota1);
+        entradaInteiro = (int) entrada / 50;
+        System.out.printf("%d nota(s) de R$ 50.00%n", entradaInteiro);
+        entrada = entrada % 50;
+
+        entradaInteiro = (int) entrada / 20;
+        System.out.printf("%d nota(s) de R$ 20.00%n", entradaInteiro);
+        entrada = entrada % 20;
+
+        entradaInteiro = (int) entrada / 10;
+        System.out.printf("%d nota(s) de R$ 10.00%n", entradaInteiro);
+        entrada = entrada % 10;
+
+        entradaInteiro = (int) entrada / 5;
+        System.out.printf("%d nota(s) de R$ 5.00%n", entradaInteiro);
+        entrada = entrada % 5;
+
+        entradaInteiro = (int) entrada / 2;
+        System.out.printf("%d nota(s) de R$ 2.00%n", entradaInteiro);
+        entrada = entrada % 2;
+
+
+
+        System.out.println("MOEDAS:");
+        entradaInteiro = (int) entrada;
+        System.out.printf("%d moeda(s) de R$ 1.00%n", entradaInteiro);
+        entrada = entrada % 1;
+
+        entrada = entrada * 100;
+
+        entradaInteiro = (int) entrada / 50;
+        System.out.printf("%d moeda(s) de R$ 0.50%n", entradaInteiro);
+        entrada = entrada % 50;
+
+        entradaInteiro = (int) entrada / 25;
+        System.out.printf("%d moeda(s) de R$ 0.25%n", entradaInteiro);
+        entrada = entrada % 25;
+
+        entradaInteiro = (int) entrada / 10;
+        System.out.printf("%d moeda(s) de R$ 0.10%n", entradaInteiro);
+        entrada = entrada % 10;
+
+        entradaInteiro = (int) entrada / 5;
+        System.out.printf("%d moeda(s) de R$ 0.05%n", entradaInteiro);
+        entrada = entrada % 5;
+
+        System.out.printf("%.0f moeda(s) de R$ 0.01%n", entrada);
+
+
         sc.close();
     }
 }
